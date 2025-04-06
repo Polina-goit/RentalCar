b1ab1df031febb91ebf38d713230c4919f214518
Polina <polinakoctyu4enko@gmail.com>
2025-04-06 23:12:36 +0300

    add Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import NavBar from './NavBar/NavBar.jsx'
 import { lazy } from 'react'
-
+import Layout from './Layout/Layout.jsx';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
@@ -16,21 +16,19 @@ function App() {
   
 
   return (
-    <>
-    
-     <div><NavBar/>
-     
+      <div>
+        <Layout>
+      <NavBar/>
       <Routes>
-        
         <Route index element={<HomePage />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/catalog/:id" element={<CarCard />} />
         <Route path="*" element={<NotFound />} />
-      
+
       </Routes>
-     
+      </Layout>
     </div>
-    </>
+  
   )
 }
 
