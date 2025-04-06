f10f0a9319fe1fe08bdfb11c13ecd8010aacc70c
Polina <polinakoctyu4enko@gmail.com>
2025-04-06 23:34:24 +0300

    update Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./NavBar/NavBar.jsx";
+import { lazy } from "react";
 
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import NavBar from './NavBar/NavBar.jsx'
-import { lazy } from 'react'
-import Layout from './Layout/Layout.jsx';
-
-const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const Catalog = lazy(() => import('../pages/Catalog/Catalog'));
-const CarCard = lazy(() =>
-  import('../pages/Car/CarCard')
-);
-const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const Catalog = lazy(() => import("../pages/Catalog/Catalog"));
+const CarCard = lazy(() => import("../pages/Car/CarCard"));
+const NotFound = lazy(() => import("../pages/NotFound/NotFound"));
 
 function App() {
-  
-
   return (
-      <div>
-        <Layout>
-      <NavBar/>
+    <div>
+      <NavBar />
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/catalog/:id" element={<CarCard />} />
         <Route path="*" element={<NotFound />} />
-
       </Routes>
-      </Layout>
     </div>
-  
-  )
+  );
 }
+y;
 
-export default App
+export default App;
